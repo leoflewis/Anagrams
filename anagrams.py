66eb6647991ba241beea38004ff9d6b8ae2ea4b7
leoflewis <53980438+leoflewis@users.noreply.github.com>
2020-05-17 15:43:52 -0500

    Changed structure

--- anagrams.py
+++ anagrams.py
@@ -4,12 +4,12 @@ import load_dictionary
 def main():
     word_list = load_dictionary.load('dictionary.txt')
     word_one = input("Hello friend, please enter a word:")
-    sorted_one = sorted(list(word_one))
+    sorted_one = sorted(word_one)
     anagram_list = []
     for word in word_list:
         word = word.lower()
         if word != word_one:
-            if sorted(list(word)) == sorted_one:
+            if sorted(word) == sorted_one:
                 anagram_list.append(word)  
     if len(anagram_list) == 0:
         print("None Found")
